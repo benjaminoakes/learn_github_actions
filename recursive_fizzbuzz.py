6c5610e54dd41b2c209066568a40791bbf40de61
Ben Oakes <hello@benjaminoakes.com>
2021-05-05 17:42:44 -0500

    Create recursive_fizzbuzz.py

---
+++ recursive_fizzbuzz.py
@@ -0,0 +1,2 @@
+# https://www.quora.com/What-are-some-prime-examples-of-bad-python-code
+(lambda f, n: f(f, n))(lambda f, n: [(not n % 3 and "fizz" or "") + (not n % 5 and "buzz" or "") or n] + f(f, n+1) if n <= 100 else [], 1) 
